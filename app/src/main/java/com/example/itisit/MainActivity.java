f2246162da2acf7995bc5a6012b66134e999a13b
soni1541 <kurbatovasoni5@gmail.com>
2023-10-19 16:56:51 +0500

    Частично сделаны экраны Главная и О приложении

--- app/src/main/java/com/example/itisit/MainActivity.java
+++ app/src/main/java/com/example/itisit/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.itisit;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+
+    private HomeFragment homeFragment = new HomeFragment();
+    private CourseFragment courseFragment = new CourseFragment();
+    private AboutFragment aboutFragment = new AboutFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.home_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, homeFragment)
+                                .commit();
+                        return true;
+
+                    case R.id.current_course_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, courseFragment)
+                                .commit();
+                        return true;
+                    case R.id.about_btn:
+
+                        getSupportFragmentManager()
+                                .beginTransaction()
+                                .replace(R.id.fragment1, aboutFragment)
+                                .commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+        bottomNavigationView.setSelectedItemId(R.id.home_btn);
     }
 }
