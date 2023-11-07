e3bd57a0776061799a31e486ecefd291932d817f
soni1541 <kurbatovasoni5@gmail.com>
2023-11-07 21:01:55 +0500

    Сделан функционал теста для курса 1

--- app/src/main/java/com/example/itisit/Course1_Test_Fragment.java
+++ app/src/main/java/com/example/itisit/Course1_Test_Fragment.java
@@ -1,9 +1,12 @@
 package com.example.itisit;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 
+import android.text.style.QuoteSpan;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +17,11 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -41,106 +48,104 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
     private ImageButton b_forward;
     private ImageButton b_back;
     private Button b_check;
-
     private ImageButton b_theory;
 
-    private String[] test_headers;
-    private String[][] test_questions;
+    private List<Question> questions;
 
-    private int[][] test_answers;
+    private List<CheckBox> checkBoxes;
+    private List<RadioButton> radioButtons;
 
     private int current_index_question;
 
     private ImageView image_result;
     private TextView text_check_answer;
 
+
+    private ConstraintLayout constraintLayout;
+
+
     public Course1_Test_Fragment() {
         // Required empty public constructor
 
-        test_headers = new String[]{
-                //0
-                "В чём плюсы С++?",
-                //1
-                "Один из основных инструментов, которым пользуются программисты:",
-                //2
-                "В каком файле находиться исходный код?",
-                //3
-                "Как выглядит начало любой программы?",
-                //4
-                "Каждая инструкция должна заканчиваться:",
-                //5
-                "Где правильно написана функция ввода?",
-                //6
-                "Выберите однострочный комментарий:",
+        questions = new ArrayList<Question>();
+
+        Question q1 = new Question();
+        q1.header = "В чём плюсы С++?";
+        q1.text_answers = new ArrayList<String>();
+        q1.text_answers.add("Универсальность");
+        q1.text_answers.add("Производительность");
+        q1.text_answers.add("Можно писать как низкоуровневые, так и высокоуровневые коды");
+        q1.text_answers.add("Использование готовых библиотек и конструкций");
+        q1.id_answers = new ArrayList<Integer>();
+        q1.id_answers.add(0);
+        q1.id_answers.add(1);
+        q1.id_answers.add(2);
+        q1.id_answers.add(3);
+        questions.add(q1);
+
+        Question q2 = new Question();
+        q2.header = "Один из основных инструментов, которым пользуются программисты:";
+        q2.text_answers = new ArrayList<String>();
+        q2.text_answers.add("Драйверы");
+        q2.text_answers.add("Среды разработки (IDE)");
+        q2.text_answers.add("Утилиты");
+        q2.id_answers = new ArrayList<Integer>();
+        q2.id_answers.add(1);
+        questions.add(q2);
+
+        Question q3 = new Question();
+        q3.header = "В каком файле находиться исходный код?";
+        q3.text_answers = new ArrayList<String>();
+        q3.text_answers.add("source.cpp");
+        q3.text_answers.add("windows.h");
+        q3.text_answers.add("source.h");
+        q3.id_answers = new ArrayList<Integer>();
+        q3.id_answers.add(0);
+        questions.add(q3);
+
+        Question q4 = new Question();
+        q4.header = "Как выглядит начало любой программы?";
+        q4.text_answers = new ArrayList<String>();
+        q4.text_answers.add("include<iostream>\nusing namespace;\n int main()\n{\n}");
+        q4.text_answers.add("double main()\n{\n}");
+        q4.text_answers.add("#include<iostream>\nusing namespace std;\n int main()\n{\n}");
+        q4.id_answers = new ArrayList<Integer>();
+        q4.id_answers.add(2);
+        questions.add(q4);
+
+        Question q5 = new Question();
+        q5.header = "Каждая инструкция должна заканчиваться:";
+        q5.text_answers = new ArrayList<String>();
+        q5.text_answers.add(";");
+        q5.text_answers.add(":");
+        q5.text_answers.add(".");
+        q5.id_answers = new ArrayList<Integer>();
+        q5.id_answers.add(0);
+        questions.add(q5);
+
+        Question q6 = new Question();
+        q6.header = "Где правильно написана функция ввода?";
+        q6.text_answers = new ArrayList<String>();
+        q6.text_answers.add("int a;\ncin a;");
+        q6.text_answers.add("int a;\ncin >> a;");
+        q6.text_answers.add("int a;\ncin << a;");
+        q6.id_answers = new ArrayList<Integer>();
+        q6.id_answers.add(1);
+        questions.add(q6);
+
+        Question q7 = new Question();
+        q7.header = "Выберите однострочный комментарий:";
+        q7.text_answers = new ArrayList<String>();
+        q7.text_answers.add("## я комментарий");
+        q7.text_answers.add("** я комментарий");
+        q7.text_answers.add("// я комментарий");
+        q7.id_answers = new ArrayList<Integer>();
+        q7.id_answers.add(2);
+        questions.add(q7);
 
 
-        };
-
-        test_questions = new String[][]
-        {
-                //0
-                {
-                    "Универсальность",
-                    "Производительность",
-                    "Можно писать как низкоуровневые, так и высокоуровневые коды",
-                    "Использование готовых библиотек и конструкций",
-                },
-                //1
-                {
-                    "Драйверы",
-                    "Среды разработки (IDE)",
-                    "Утилиты"
-                },
-                //2
-                {
-                    "source.cpp",
-                    "windows.h",
-                    "source.h"
-                },
-                //3
-                {
-                    "include<iostream>\nusing namespace;\n int main()\n{\n}",
-                    "int main()\n{\n}",
-                    "#include<iostream>\nusing namespace std;\n int main()\n{\n}"
-                },
-                //4
-                {
-                        ";",
-                        ":",
-                        "."
-                },
-                //5
-                {
-                    "int a;\ncin a;",
-                    "int a;\ncin >> a;",
-                    "int a;\ncin << a;"
-                },
-                //6
-                {
-                        "## я комментарий",
-                        "** я комментарий",
-                        "// я комментарий"
-                }
-        };
-
-        test_answers = new int[][]
-        {
-                //0
-                {0, 1, 2, 3},
-                //1
-                {1},
-                //2
-                {0},
-                //3
-                {2},
-                //4
-                {0},
-                //5
-                {1},
-                //6
-                {2}
-
-        };
+        checkBoxes = new ArrayList<CheckBox>();
+        radioButtons = new ArrayList<RadioButton>();
 
     }
 
@@ -191,21 +196,25 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
         image_result = (ImageView) view.findViewById(R.id.imageView_result);
         text_check_answer = (TextView) view.findViewById(R.id.textView_check_answer);
 
+        constraintLayout = (ConstraintLayout) view.findViewById(R.id.layout_test_course1);
+
         show_action(view);
 
 
         b_forward.setOnClickListener(this);
         b_back.setOnClickListener(this);
         b_theory.setOnClickListener(this);
+        b_check.setOnClickListener(this);
 
         return view;
     }
 
     public void show_action(View view){
+
         if(current_index_question == 0)
         {
-            //b_back.setVisibility(View.INVISIBLE);
-            //b_forward.setVisibility(View.INVISIBLE);
+            b_back.setVisibility(View.INVISIBLE);
+            b_forward.setVisibility(View.INVISIBLE);
 
             text_check_answer.setVisibility(View.INVISIBLE);
 
@@ -213,29 +222,53 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
             image_result.setVisibility(View.GONE);
         }
 
+        Question question_now = questions.get(current_index_question);
+
+
         // Отображение теста (заголовка и выбора ответов)
         if(current_index_question >= 0 &&
-                current_index_question < test_headers.length)
+                current_index_question < questions.size())
         {
-            header_test.setText(test_headers[current_index_question]);
+            b_check.setVisibility(View.VISIBLE);
+            b_check.setText("ПРОВЕРИТЬ");
+            b_check.setBackgroundColor(Color.BLACK);
+            text_check_answer.setVisibility(View.INVISIBLE);
+            b_back.setVisibility(View.INVISIBLE);
+            b_forward.setVisibility(View.INVISIBLE);
+            constraintLayout.setBackgroundColor(Color.parseColor("#5B9C34"));
+            image_result.setVisibility(View.INVISIBLE);
 
-            for(int answer = 0; answer < test_questions[current_index_question].length; answer ++)
+
+            header_test.setText(question_now.header);
+
+            RadioGroup radioGroup = new RadioGroup(this.getContext());
+
+            for(int answer = 0; answer < question_now.text_answers.size(); answer ++)
             {
-                String text_question = test_questions[current_index_question][answer];
-                if(test_answers[current_index_question].length > 1) {
+                String text_question = question_now.text_answers.get(answer);
+                if(question_now.id_answers.size() > 1) {
                     CheckBox checkBox_question = new CheckBox(getContext());
+                    checkBox_question.setId(answer);
                     checkBox_question.setText(text_question);
+                    checkBoxes.add(checkBox_question);
                     content_question_test.addView(checkBox_question);
                 }
-                else if (test_answers[current_index_question].length == 1) {
+                else if (question_now.id_answers.size() == 1) {
+
                     RadioButton radioButton_question = new RadioButton(getContext());
+                    radioButton_question.setId(answer);
                     radioButton_question.setText(text_question);
-                    content_question_test.addView(radioButton_question);
+                    if(radioGroup.getParent() != null) {
+                        ((ViewGroup)radioGroup.getParent()).removeView(radioGroup); // <- fix
+                    }
+                    radioGroup.addView(radioButton_question);
+                    radioButtons.add(radioButton_question);
+                    content_question_test.addView(radioGroup);
                 }
             }
 
         }
-        else if (current_index_question == test_headers.length)
+        else if (current_index_question == questions.size())
         {
             // завершить тест
         }
@@ -253,7 +286,9 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
 
 
                 content_question_test.removeAllViews();
-                ++current_index_question;
+                checkBoxes.clear();
+                radioButtons.clear();
+                current_index_question++;
 
                 //Отображение положений кнопок
                 show_action(view);
@@ -264,7 +299,9 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
 
 
                 content_question_test.removeAllViews();
-                --current_index_question;
+                checkBoxes.clear();
+                radioButtons.clear();
+                current_index_question--;
 
                 //Отображение положений кнопок
                 show_action(view);
@@ -273,7 +310,99 @@ public class Course1_Test_Fragment extends Fragment implements View.OnClickListe
             }
             case R.id.button_check: {
 
+                Log.d("START", "START CHECK");
+                boolean result = true;
 
+                Question question_now = questions.get(current_index_question);
+                Log.d("ANSWER size", String.valueOf(question_now.id_answers.size()));
+
+                if(question_now.id_answers.size() > 1)
+                {
+                    Log.d("CHECKBOX", "Я в боксе");
+                    for (CheckBox checkBox_question: checkBoxes)
+                    {
+                        int id_check_now = checkBox_question.getId();
+                        if(checkBox_question.isChecked())
+                        {
+                            //Log.d("ID_CHECK_BOX", String.valueOf(checkBox_question.getId()));
+                            if(questions.get(current_index_question).id_answers.contains(id_check_now))
+                            {
+                                //Правильно
+                            }
+                            else
+                            {
+                                //Неправильно
+                                result = false;
+                                break;
+                            }
+                        }
+                        else
+                        {
+                            if(!questions.get(current_index_question).id_answers.contains(id_check_now))
+                            {
+                                //Правильно
+                            }
+                            else
+                            {
+                                //Неправильно
+                                result = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+                else if(question_now.id_answers.size() == 1)
+                {
+                    for (RadioButton radioButton_question: radioButtons) {
+                        int id_check_now = radioButton_question.getId();
+                        Log.d("RADIO size", String.valueOf(radioButtons.size()));
+                        if (radioButton_question.isChecked()) {
+                            Log.d("ID_RADIO", String.valueOf(id_check_now));
+                            if (questions.get(current_index_question).id_answers.contains(id_check_now)) {
+                                //Правильно
+                                //b_check.setVisibility(View.INVISIBLE);
+                            } else {
+                                //Неправильно
+                                result = false;
+                                break;
+                            }
+                        } else {
+                            if (!questions.get(current_index_question).id_answers.contains(id_check_now)) {
+                                //Правильно
+                            } else {
+                                //Неправильно
+                                result = false;
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if(result)
+                {
+                    b_check.setVisibility(View.INVISIBLE);
+                    text_check_answer.setText("Всё верно! Молодец!\nДвигаемся дальше");
+                    text_check_answer.setTextColor(Color.parseColor("#5B9C34"));
+                    text_check_answer.setVisibility(View.VISIBLE);
+
+                    if(current_index_question > 0)
+                        b_back.setVisibility(View.VISIBLE);
+                    b_forward.setVisibility(View.VISIBLE);
+
+                    image_result.setImageResource(R.drawable.correct);
+                    image_result.setVisibility(View.VISIBLE);
+
+                    constraintLayout.setBackgroundColor(Color.parseColor("#5B9C34"));
+                }
+                else
+                {
+                    b_check.setText("Проверить ещё раз");
+                    b_check.setBackgroundColor(Color.parseColor("#F35D78"));
+
+                    image_result.setImageResource(R.drawable.incorrect);
+                    image_result.setVisibility(View.VISIBLE);
+                    constraintLayout.setBackgroundColor(Color.parseColor("#F35D78"));
+                }
 //
             }
             case R.id.imageButton_theory: {
---
+++ app/src/main/java/com/example/itisit/Question.java
@@ -0,0 +1,10 @@
+package com.example.itisit;
+
+import java.util.List;
+
+public class Question
+{
+    String header;
+    List<String> text_answers;
+    List<Integer> id_answers;
+}
